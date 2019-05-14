c89196ddc12a349454995bc1c8fef59c91a99dbe
digant-dubey <dubeydigant@gmail.com>
2019-05-14 20:04:49 +0530

    new

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  timestamp: Date = new Date();
+  offset = '+0700'
+}
